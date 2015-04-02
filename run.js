8ce531a3e8e0ca64cc393dcac46449b48f99c66d
dibakar-datta <dibakar.datta@gmail.com>
2015-04-02 12:56:57 +0530

    2nd node program
    2nd node program

---
+++ run.js
@@ -0,0 +1,5 @@
+// to use methods from other files we simply use 'require' with path name
+var reader = require('./read'),
+	writer = require('./write');
+
+// call 'read' method from read.js to read 
